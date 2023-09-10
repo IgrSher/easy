f876cb85ce667d43f1eb7c8d9c9a4baa553051f2
IgrSher <143996864+IgrSher@users.noreply.github.com>
2023-09-10 22:26:58 +0300

    Update module.cpp

--- module.cpp
+++ module.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "module.h"
+#include "easy.h"
 void itc_fio(void) {
 	std::cout << "Sher ";
 	itc_name();
@@ -17,13 +17,15 @@ int itc_abs(int num) {
 	return f;
 }
 double itc_fabs(double num) {
-	double f = fabs(num);
-	return f;
+	if (num > 0)
+		return num;
+	else
+		return -num;
 }
 int itc_revnbr(int num) {
 	int a, b, c;
 	a = num % 10;
-	b = (num % 100 - a)/10;
+	b = (num % 100 - a) / 10;
 	c = num / 100;
 	return (a * 100) + (b * 10) + c;
 }
@@ -35,4 +37,66 @@ int itc_max(int num1, int num2) {
 		return num1;
 	else
 		return num2;
+}
+int itc_min(int min1, int min2) {
+	if (min1 <  min2)
+		return min1;
+	else
+		return min2;
+}
+double itc_fmax(double num, double num2) {
+	if (num > num2)
+		return num;
+	else
+		return num2;
+}
+double itc_fmin(double num, double num2) {
+	if (num < num2)
+		return num;
+	else
+		return num2;
+}
+int itc_sqrt(int num) {
+	for (int i = 0; i * i <= num; i++) {
+		if (i * i == num)
+			return num;
+	}
+	return -1;
+}
+int itc_skv(int num) {
+	if (num > 0)
+		return num * num;
+	return -1;
+}
+int itc_spr(int a, int b) {
+	if (a > 0 && b > 0)
+		return a * b;
+	return -1;
+}
+int itc_str(int a, int b, int c) {
+	if ((a > 0 && b > 0 && c > 0) && ((a < b + c) && (b < a + c) && (c < a + b))) {
+		double p = (a + b + c) / 2;
+		return itc_sqrt(p * (p - a) * (p - b) * (p - c));
+	}
+	return -1;
+}
+double itc_scir(int radius) {
+	if (radius > 0)
+		return radius * 3.14;
+	return -1;
+}
+double itc_pow(int num, int step) {
+	int numnum = 1;
+	for (int i = 0; i < itc_abs(step); i++)
+		numnum = numnum * num;
+	if (step < 0)
+		return 1.0 / numnum;
+	else
+		return numnum;
+}
+bool itc_ispositive(int num) {
+	return num >= 0;
+}
+bool itc_ispositive_d(double num) {
+	return num >= 0;
 }
